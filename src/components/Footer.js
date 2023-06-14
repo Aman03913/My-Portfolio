8620bf07510f87e0f77742099b6d1499491ff080
Aman Shishodia <aman2021cs039@abesit.edu.in>
2023-06-14 14:52:57 +0530

    changes updated

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,16 +3,19 @@ import InstagramIcon from "@material-ui/icons/Instagram";
 import TwitterIcon from "@material-ui/icons/Twitter";
 import FacebookIcon from "@material-ui/icons/Facebook";
 import LinkedInIcon from "@material-ui/icons/LinkedIn";
+
+import GithubIcon from "@material-ui/icons/GitHub";
 import "../styles/Footer.css";
 
 function Footer() {
   return (
     <div className="footer">
       <div className="socialMedia">
-         <InstagramIcon />
-        <TwitterIcon />
-        <FacebookIcon />
-        <LinkedInIcon />
+         <a href="https://www.instagram.com/aman_shishodia_007/?next=%2F" target="blank"><InstagramIcon /></a>
+        <a href="https://twitter.com/home"><TwitterIcon /></a>
+        <a href="https://www.facebook.com/"></a><FacebookIcon />
+        <a href="https://www.linkedin.com/in/aman-shishodia-601a09221"><LinkedInIcon /></a>
+        <a href="https://github.com/"><GithubIcon /></a>
       </div>
       <p> &copy; 2022 amanportfolio7.com</p>
     </div>
